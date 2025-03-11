a79b9156acc88d7361032c20e5be7db5e1bc9f20
lfd <2281409362@qq.com>
2025-03-11 20:56:08 +0800

    pthread_cancel.c

---
+++ pthread_cancel.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<error.h>
+void *tfn(void *arg)
+{
+    while(1)
+    {
+        printf("tfn:%lu\n",pthread_self());//获取线程id
+        printf("%d\n",getpid());
+        sleep(1);
+    }
+    return NULL;
+}
+int main()
+{    
+    pthread_t tid;
+    //创建子线程
+    int ret=pthread_create(&tid,NULL,tfn,NULL);
+    // if(ret!=0)
+    // {
+    //     fprintf(stderr,"err:%s\n",strerror((ret)));
+    // }
+    sleep(5);
+    ret=pthread_cancel(tid);
+    // if(ret!=0)
+    // {
+    //     fprintf(stderr,"err:%s\n",strerror(ret));
+    // }
+    printf("main:%lu\n",pthread_self());//获取线程id
+    printf("%d\n",getpid());
+    sleep(1);//给子线程执行时间
+    return 0;//释放进程地址空间
+}
